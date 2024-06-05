6347fbdb3432db61cff81b7f5b38a20d20d698ae
Jose Rojas <jgra11.2010@gmail.com>
2024-06-05 11:55:30 -0400

    Props added to Button component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,15 +1,21 @@
-<script setup></script>
+<script setup>
+defineProps({
+	styles: String
+})
+</script>
 
 <template>
 	<div class="flex justify-start w-44 relative">
 		<button
-			class="py-1 px-4 text-2xl border-3 font-bold w-44 transform hover:-translate-y-1 hover:-translate-x-1 z-10 transition-transform duration-100 border-white"
+			class="py-1 px-4 text-2xl border-3 font-bold w-44 transform hover:-translate-y-1 hover:-translate-x-1 z-10 transition-transform duration-100"
+			:class="styles"
 		>
 			Ver más
 		</button>
 
 		<div
-			class="h-full w-full text-xl border-r-3 border-b-3 borderColor absolute border-white"
+			class="h-full w-full text-xl border-r-3 border-b-3 borderColor absolute"
+			:class="styles"
 		></div>
 	</div>
 </template>
